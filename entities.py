8dd42b42e54c7d3c64a347ba79893122967e60a8
Stanislav Lemeshonok <stlemeshonok@rc-spb.renaissancecreation.com>
2025-11-05 12:52:15 +0300

    First playable version: combat system and upgrades

--- entities.py
+++ entities.py
@@ -1,15 +1,17 @@
 class Player:
-    def __init__(self, player_name, player_blood, player_kills, player_damage):
+    def __init__(self, player_name, player_blood, player_kills, player_damage, player_health):
         self.player_name = player_name
         self.player_blood = player_blood
         self.player_kills = player_kills
         self.player_damage = player_damage
+        self.player_health = player_health
 
     def show_player_stats(self):
         print(f'Player name: {self.player_name} \n'
               f'Blood count: {self.player_blood} \n'
               f'Kills: {self.player_kills} \n'
-              f'Damage: {self.player_damage}')
+              f'Damage: {self.player_damage}\n'
+              f'HP: {self.player_health}')
 
     def player_attack(self, mob):
         if mob.health > 0:
@@ -18,8 +20,13 @@ class Player:
             self.player_blood += self.player_damage
             print(f'{mob.name} have {mob.health} HP left!\n')
 
+    def take_damage(self, damage):
+        self.player_health -= damage
+        if self.player_health <= 0:
+            return "Death"
+
     def buy_upgrade(self):
-        print(f'Which upgrade you want?'
+        print(f'Which upgrade you want? '
               f'You have {self.player_blood} for updates')
         print(f'1) Increase your damage: +1   (cost 2 blood)\n'
               f'2) Increase your damage: +2   (cost 5 blood)\n'
@@ -52,3 +59,9 @@ class Mob:
         self.health -= damage
         if self.health <= 0:
             return "Death"
+
+    def mob_attack(self, player):
+        if player.player_health > 0:
+            print(f'{self.name} has attack {player.player_name} at {self.damage}')
+            player.take_damage(self.damage)
+            print(f'{player.player_name} have {player.player_health} HP left!\n')
--- main.py
+++ main.py
@@ -4,33 +4,42 @@ import random
 
 
 def fight_with_enemy(player, enemy):
-    print('Fight is start! '
-          f"Enemy: {enemy.name} (HP: {enemy.health})\n")
-    while enemy.health > 0:
-        player.player_attack(enemy)
-    player.player_kills += 1
-    print(f'{enemy.name} is Dead!')
+    if enemy.health <= 0:
+        print(f'\n{enemy.name} already dead!\n')
+    else:
+        print('Fight is start! '
+              f'Enemy: {enemy.name} (HP: {enemy.health})\n')
+        while enemy.health > 0 and player.player_health > 0:
+            player.player_attack(enemy)
+            enemy.mob_attack(player)
+        if enemy.health <= 0:
+            player.player_kills += 1
+            print(f'{enemy.name} is Dead!')
+        else:
+            print('YOU DIED\n')
+            return False
 
 
 def game_loop():
-    player1 = Player('Guts', 0, 0, 1)
+    player1 = Player('Guts', 0, 0, 1, 10)
 
-    while True:
+    while player1.player_health > 0:
         print(f'1) Show all enemy\n'
               f'2) Attack random enemy\n'
               f'3) Update shop\n'
               f'4) Show stats\n'
               f'5) End game')
         player_choice = int(input('Your choice: '))
+        print('\n')
         if player_choice == 1:
-            while True:
+            while player1.player_health > 0:
                 for i, enemy in enumerate(basic_enemies, 1):
                     if enemy.health <= 0:
                         print(f'{enemy.name} enemy already died')
                     else:
                         print(f'{i}) {enemy.name} (HP: {enemy.health})')
                 print("Print 9 to exit to main menu")
-                enemy_choice = int(input('Choice enemy to kill: ')) - 1
+                enemy_choice = int(input('Choice enemy to kill: \n')) - 1
                 if 0 <= enemy_choice < len(basic_enemies):
                     fight_with_enemy(player1, basic_enemies[enemy_choice])
                 elif enemy_choice == 8:
@@ -52,6 +61,7 @@ def game_loop():
             break
         else:
             print('Unwrite choice')
+
 
 
 if __name__ == "__main__":
